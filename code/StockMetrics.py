a7896ead14e28156d99198da32549e067ee47546
jacob <jacoblam87@gmail.com>
2023-10-29 01:49:13 -0400

    Calculating the average, median, standard deviation of amazon stock prices using code verification.

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -18,16 +18,52 @@ class StockMetrics(StockData):
         """
         averages = []
         for row in self.data:
-            print(row)
-
+            prices = []
+            for val in row [1:]:
+                #print(type(val))
+                try:
+                    float(val)
+                    prices.append(float(val))
+                except:
+                    continue 
+            
+            calculated_averages = round(sum(prices) / len(prices), 3)
+            averages.append(calculated_averages)
+        print(averages)
         return averages
 
     def median02(self):
         """pt2
         """
-        ...
+        median = []
+        for row2 in self.data:
+            prices2 = []
+            for val2 in row2 [1:]:
+                try:
+                    float(val2)
+                    prices2.append(float(val2))
+                except:
+                    continue
+            calculated_median = stats.median(prices2)
+            median.append(calculated_median)
+        print(median)
+        return median
+
+        
 
     def stddev03(self):
         """pt3
         """
-        ...
+        standarddeviation = []
+        for row3 in self.data:
+            prices3 = []
+            for val3 in row3 [1:]:
+                try:
+                    float(val3)
+                    prices3.append(float(val3))
+                except:
+                    continue
+            calculated_median = stats.stdev(prices3)
+            standarddeviation.append(round(calculated_median, 3))
+        print(standarddeviation)
+        return standarddeviation
