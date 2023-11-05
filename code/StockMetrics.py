7cbb4aacd4919e232d928786e8cd516968ac3967
jacob <jacoblam87@gmail.com>
2023-11-05 17:08:23 -0500

    Adding in documentation for our StockMetrics code to make it easier for people to understand.

--- code/StockMetrics.py
+++ code/StockMetrics.py
@@ -19,18 +19,27 @@ class StockMetrics(StockData):
         averages = []
         for row in self.data:
             prices = []
+        # Here we are creating a new variable called prices so we can append our filtered clean data into this new variable later.
             for val in row [1:]:
-                #print(type(val))
+        # Using a control flow statement we want to filter out val in our row of data to only include data from 1 til the end.
+        # what this means esentially is that from our data we want to skip or filter out the date and keep the rest of our data.
                 try:
                     float(val)
                     prices.append(float(val))
                 except:
                     continue 
-            
+        # The try and except function will make sure that we are going to try and convert all of our values from a string to a float.
+        # After we convert our data from a string to a float we will append them into the prices variable list that we have created.
+        # If we encounter something that can't be converted into a float like " " empty spaces we will simply ignore and continue it
+        # thus filtering out spaces in our data.
+    
             calculated_averages = round(sum(prices) / len(prices), 3)
             averages.append(calculated_averages)
         print(averages)
         return averages
+    
+        # Since our validation code only accepts numbers rounded to the third decimal we must also round our prices to the third decimal.
+        # We will calculate our average using the average function which is the sum of all prices divided by the amount of numbers we have.
 
     def median02(self):
         """pt2
@@ -49,6 +58,8 @@ class StockMetrics(StockData):
         print(median)
         return median
 
+        # The median follows the same principle as the code we have used above for average. The only difference is instead we are using the stat module
+        # to calculate the median for us.
         
 
     def stddev03(self):
@@ -67,3 +78,6 @@ class StockMetrics(StockData):
             standarddeviation.append(round(calculated_median, 3))
         print(standarddeviation)
         return standarddeviation
+
+        # The standard deviation follows the same principle as the code we have used above for average. The only difference is instead we are using the stat module
+        # to calculate the standard deviation for us.
